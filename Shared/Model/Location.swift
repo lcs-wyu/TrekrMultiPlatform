cccb7ba6098de4427d6c39e77a5a2aee919f9df9
Wellesley Yu <byu@lcs.on.ca>
2021-01-26 09:06:33 -0500

    Created a structure for describing locations.

---
+++ Shared/Model/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  TrekrMultiPlatform
+//
+//  Created by Yu, Boning on 2021-01-26.
+//
+
+import Foundation
+
+struct Location {
+    let Id : Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let advisory: String
+}
