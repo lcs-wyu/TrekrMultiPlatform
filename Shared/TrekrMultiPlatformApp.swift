288b840d2373d814aba8f10488e6d6b3fde3f30a
Wellesley Yu <byu@lcs.on.ca>
2021-01-26 08:46:13 -0500

    Initial Commit

---
+++ Shared/TrekrMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlatformApp.swift
+//  Shared
+//
+//  Created by Yu, Boning on 2021-01-26.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
